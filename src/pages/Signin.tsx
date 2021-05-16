6506ac4935c4aa565983d7f1a18d5b4164e2ad4e
Laurent Schaffner <laurent.schaffner.code@gmail.com>
2021-05-16 12:54:19 -0600

    feat: signin module

--- src/pages/Signin.tsx
+++ src/pages/Signin.tsx
@@ -1,32 +1,64 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonRow, IonCol, IonItem, IonLabel, IonInput } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonRow, IonCol, IonItem, IonLabel, IonInput, IonIcon, IonButton } from '@ionic/react';
+import React, { useState } from 'react';
 import './Signin.css';
 const Signin: React.FC = () => {
+
+  const [email, setEmail] = useState<string>();
+  const [password, setPassword] = useState<string>();
+
   return (
     <IonPage>
       <IonHeader>
-        <IonToolbar>
-          <IonTitle>Sign-in</IonTitle>
+        <IonToolbar color="primary">
+          <IonTitle slot="end">
+            Sign-in
+          </IonTitle>
+          <IonTitle slot="start" size="large">
+            <IonIcon src="assets/icon/logo-reversed.svg" size="large" style={{ verticalAlign: "bottom" }}></IonIcon>
+          </IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Signin</IonTitle>
+            <IonTitle size="large">Sign-in</IonTitle>
           </IonToolbar>
         </IonHeader>
-          <IonRow className="ion-justify-content-center">
-            <IonCol sizeXs="10" sizeMd="5">
-              <IonItem>
-                <IonLabel position="floating"> Email</IonLabel>
-                <IonInput
-                  type="email"
-                  value=""
+        <IonRow className="ion-justify-content-center">
+          <IonCol sizeXs="10" sizeMd="5">
+
+            <IonRow>
+              <IonCol>
+                <IonItem>
+                  <IonLabel position="floating"> Email</IonLabel>
+                  <IonInput
+                    type="email"
+                    value={email}
+                    onIonChange={e => setEmail(e.detail.value!)}
                   >
-                </IonInput>
-              </IonItem>
-            </IonCol>
-          </IonRow>
+                  </IonInput>
+                </IonItem>
+                <IonItem>
+                  <IonLabel position="floating"> Password</IonLabel>
+                  <IonInput
+                    type="password"
+                    value={password}
+                    onIonChange={e => setPassword(e.detail.value!)}
+                  >
+                  </IonInput>
+                </IonItem>
+              </IonCol>
+            </IonRow>
+
+            <IonRow>
+              <IonCol>
+                <IonButton expand="block">Sign-in</IonButton>
+              </IonCol>
+            </IonRow>
+
+          </IonCol>
+
+        </IonRow>
       </IonContent>
     </IonPage>
   );
