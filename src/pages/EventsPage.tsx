4e3eaf46afd1c6b1f423cb775c3289989bf0783a
Laurent Schaffner <laurent.schaffner.code@gmail.com>
2021-05-18 22:07:26 -0500

    feat: dismiss loading and events on the list

--- src/pages/EventsPage.tsx
+++ src/pages/EventsPage.tsx
@@ -1,10 +1,13 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonGrid, IonCardSubtitle, IonRow, IonCol, IonItem, useIonToast, useIonLoading, IonLabel, IonInput, IonIcon, IonButton, IonAlert } from '@ionic/react';
-import React, { useState } from 'react';
+import { IonContent, IonPage, IonTitle, IonGrid, IonCardSubtitle, IonRow, IonCol, IonItem, useIonToast, useIonLoading, IonButton } from '@ionic/react'
+import React, { useState } from 'react'
 import { useQuery } from '@apollo/client'
 import MainHeaderComponent from '../components/MainHeaderComponent'
 import './EventsPage.css';
 
 import { QUERY_LIST_EVENTS } from '../queries/ListEvents'
+
+let loadingSpawned = false
+let dismissSpawned = false
 
 const EventPage: React.FC = () => {
 
@@ -16,14 +19,15 @@ const EventPage: React.FC = () => {
     }
   })
 
-  // showLoading('Loading events', 0, 'dots')
-
-  console.log(loading)
-
   if (loading) {
-    // showLoading('Loading events', 0, 'dots')
+    if (!loadingSpawned) {
+      loadingSpawned = true
+      showLoading('Loading events', 0, 'dots')
+    }
   } else {
-    dismissLoading()
+    if (!dismissSpawned) {
+      dismissSpawned = true
+    }
   }
 
   if (error) {
@@ -34,23 +38,27 @@ const EventPage: React.FC = () => {
     })
   }
 
+  if (data?.size !== 0) {
+    setTimeout(dismissLoading, 500)
+  }
+
   let events: Array<any> = []
 
   if (data?.listEvents.length > 0) {
     for (const [index, value] of data.listEvents.entries()) {
       events.push(
-        <IonRow className="body">
-        <IonCol size="2">{value.id}</IonCol>
-        <IonCol size="1">{value.method}</IonCol>
-        <IonCol size="1">{value.headers}</IonCol>
-        <IonCol size="1">{value.body}</IonCol>
-        <IonCol size="1">{value.endpoint}</IonCol>
-        <IonCol size="1">{value.scheduledFor}</IonCol>
-        <IonCol size="1">{value.enqueuedAt}</IonCol>
-        <IonCol size="1">{value.lockedAt}</IonCol>
-        <IonCol size="1">{value.dispatchedAt}</IonCol>
-        <IonCol size="2"><IonButton color="secondary">Show</IonButton><IonButton>Delete</IonButton></IonCol>
-      </IonRow>
+        <IonRow className="body" key={index}>
+          <IonCol size="2">{value.id}</IonCol>
+          <IonCol size="1">{value.method}</IonCol>
+          <IonCol size="1">{value.headers}</IonCol>
+          <IonCol size="1">{value.body}</IonCol>
+          <IonCol size="1">{value.endpoint}</IonCol>
+          <IonCol size="1">{value.scheduledFor}</IonCol>
+          <IonCol size="1">{value.enqueuedAt}</IonCol>
+          <IonCol size="1">{value.lockedAt}</IonCol>
+          <IonCol size="1">{value.dispatchedAt}</IonCol>
+          <IonCol size="2"><IonButton color="secondary">Show</IonButton><IonButton>Delete</IonButton></IonCol>
+        </IonRow>
       )
     }
   }
@@ -62,43 +70,43 @@ const EventPage: React.FC = () => {
         <IonRow className="ion-justify-content-center">
           <IonCol>
 
-          <IonRow className="ion-justify-content-left">
-          <IonCol>
+            <IonRow className="ion-justify-content-left">
+              <IonCol>
 
-            <IonItem>
-              <IonTitle>
-                Events
+                <IonItem>
+                  <IonTitle>
+                    Events
               </IonTitle>
-            </IonItem>
+                </IonItem>
 
-            <IonGrid class="ion-margin">
-              <IonRow>
-                <IonCol>
-                  <IonCardSubtitle>
-                  This list shows the last 500 events scheduled on Bloodbath.
+                <IonGrid class="ion-margin">
+                  <IonRow>
+                    <IonCol>
+                      <IonCardSubtitle>
+                        This list shows the last 500 events scheduled on Bloodbath.
                   </IonCardSubtitle>
-                </IonCol>
-              </IonRow>
-            </IonGrid>
+                    </IonCol>
+                  </IonRow>
+                </IonGrid>
 
-            <IonGrid className="ion-margin">
-              <IonRow className="header">
-                <IonCol size="2">ID</IonCol>
-                <IonCol size="1">Method</IonCol>
-                <IonCol size="1">Headers</IonCol>
-                <IonCol size="1">Body</IonCol>
-                <IonCol size="1">Endpoint</IonCol>
-                <IonCol size="1">Scheduled for</IonCol>
-                <IonCol size="1">Enqueued at</IonCol>
-                <IonCol size="1">Locked at</IonCol>
-                <IonCol size="1">Dispatched at</IonCol>
-                <IonCol size="2">Action</IonCol>
-              </IonRow>
-              {events}
-            </IonGrid>
+                <IonGrid className="ion-margin">
+                  <IonRow className="header">
+                    <IonCol size="2">ID</IonCol>
+                    <IonCol size="1">Method</IonCol>
+                    <IonCol size="1">Headers</IonCol>
+                    <IonCol size="1">Body</IonCol>
+                    <IonCol size="1">Endpoint</IonCol>
+                    <IonCol size="1">Scheduled for</IonCol>
+                    <IonCol size="1">Enqueued at</IonCol>
+                    <IonCol size="1">Locked at</IonCol>
+                    <IonCol size="1">Dispatched at</IonCol>
+                    <IonCol size="2">Action</IonCol>
+                  </IonRow>
+                  {events}
+                </IonGrid>
 
-          </IonCol>
-        </IonRow>
+              </IonCol>
+            </IonRow>
           </IonCol>
 
         </IonRow>
