18c6725b9c732e9c035cd7b707d31f2c5f0f34e6
Laurent Schaffner <laurent.schaffner.code@gmail.com>
2021-09-26 22:48:00 +0200

    feat: new links on api

--- src/helpers/externalLinks.tsx
+++ src/helpers/externalLinks.tsx
@@ -1,2 +1,2 @@
-export const betaRelease = 'https://bloodbath.notion.site/Being-on-the-beta-release-507b8a4cea51483dab37681db72cb666'
-export const gettingStartedGuide = 'https://bloodbath.notion.site/Create-your-first-event-c6544b42b42d452aa7e6f5f5a1f7d6a5'
+export const betaRelease = 'https://docs.bloodbath.io/getting-started/being-on-the-beta-release'
+export const gettingStartedGuide = 'https://docs.bloodbath.io/getting-started/acquire-your-api-key'
